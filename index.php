5f8623ecb5db4885779bd19a81fc39641babad2e
Vale <oirelav95@gmail.com>
2023-01-24 19:01:51 +0100

    aggiungo possibilità di mostrare più generi

--- index.php
+++ index.php
@@ -1,17 +1,40 @@
 <?php
 
+    class Genre {
+
+        public $genere1;
+        public $genere2;
+        public $genere3;
+        public $genere4;
+
+        function __construct($genere1, $genere2, $genere3, $genere4) {
+
+            $this -> genere1 = $genere1;
+            $this -> genere2 = $genere2;
+            $this -> genere3 = $genere3;
+            $this -> genere4 = $genere4;
+
+        }
+
+        public function getGenre() {
+            
+            return $this -> genere1 . ", " . $this-> genere2 . ", " . $this-> genere3 . ($this-> genere4 ? ", " . $this-> genere4 : "");
+
+        }
+    }
+
     class Movie {
 
         public $titolo;
         public $dataUscita;
-        public $genere;
+        public $generi;
         public $durata;
 
-        function __construct($titolo, $dataUscita, $genere, $durata) {
+        function __construct($titolo, $dataUscita, $generi, $durata) {
 
             $this -> titolo = $titolo;
             $this -> dataUscita = $dataUscita;
-            $this -> genere = $genere;
+            $this -> generi = $generi;
             $this -> durata = $durata;
 
         }
@@ -20,15 +43,16 @@
 
             return $this -> titolo .
             " (" . $this -> dataUscita . ")" .
-            "<br>Genere: " . $this -> genere . 
-            "<br>Durata: " . $this -> durata;
+            "<br>Genere: " . $this -> generi -> getGenre() . 
+            "<br>Durata: " . $this -> durata . " min";
 
         }
     }
 
     $movies = [
-        new Movie("Avatar", "2009", "fantascienza", 162),
-        new Movie("Avengers: Endgame", "2019", "fantascienza", 181),
+        new Movie("Avatar", "2009", new Genre("fantascienza", "azione", "avventura", ""), 162),
+        new Movie("Avengers: Endgame", "2019", new Genre("azione", "avventura", "fantascienza", ""), 181),
+        new Movie("Pirati dei Caraibi - La maledizione della prima luna", "2003", new Genre("avventura", "commedia", "fantastico", "azione"), 143),
     ];
 
     echo "Lista film: <br>";
