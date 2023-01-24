dacb43fe8daa2b2bada726144cd6a93874794d1c
Vale <oirelav95@gmail.com>
2023-01-24 16:09:10 +0100

    aggiungo costruttore

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
         public $dataUscita;
         public $genere;
         public $durata;
+
+        function __construct($titolo, $dataUscita, $genere, $durata) {
+
+            $this -> titolo = $titolo;
+            $this -> dataUscita = $dataUscita;
+            $this -> genere = $genere;
+            $this -> durata = $durata;
+
+        }
     }
 
 ?>
